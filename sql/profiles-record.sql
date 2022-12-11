80bb2d720671f56ee48b92058f75425b1a9d2287
Vaishnav200517918 <116231665+Vaishnav200517918@users.noreply.github.com>
2022-12-10 21:45:14 -0500

    Delete sql directory

--- sql/profiles-record.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE profiles (
-                          id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                          fullname varchar(100) NOT NULL,
-                          email varchar(100) NOT NULL,
-                          bio varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
